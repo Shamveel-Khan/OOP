f0e9b98dd739b866e05960a4f7c4b7105e6526e3
Shamveel-Khan <shamveelkhilji@gmail.com>
2025-03-25 23:03:04 +0500

    q2 complete

--- assighnment#2/q2.cpp
+++ assighnment#2/q2.cpp
@@ -12,8 +12,12 @@ class ghost {
         
     virtual ~ghost() {} 
     
+    int getScarelevel() {
+        return scareLevel;
+    }
+
     virtual void haunt() const {  
-        cout << name << " is haunting generically!" << endl;
+        cout << name << " is haunting!" << endl;
     }
     
     virtual void display() const {
@@ -77,6 +81,97 @@ public:
     }
 };
 
+class visitor {
+    string name;
+    int braveryLevel;
+    public:
+        visitor(string n,int bl):name(n),braveryLevel(bl) {}
+        void react(int g) {
+            if(g+1>braveryLevel) {
+                cout<<"\nVisitor ran away!!\n";
+            }
+            else if(g+1<braveryLevel) {
+                cout<<"\nVisitor laughs!!\n";
+            }
+            else {
+                cout<<"\nVisitor Stays!!\n";
+            }
+        }
+        void display() const {
+            cout << "\n.................\n";
+            cout<<"Name: "<<name<<endl;
+            cout<<"Bravery Level: "<<braveryLevel<<endl;
+            cout << "\n.................\n";
+        }
+};
+
+class hauntedHouse {
+    string name;
+    ghost** listGhosts;
+    int ghostCount;
+    int TotalGhostCapacity;
+    public:
+        hauntedHouse(string n,int tgc): name(n),TotalGhostCapacity(tgc) {
+            listGhosts = new ghost*[TotalGhostCapacity];
+            ghostCount=0;
+        }
+        void addGhost(ghost* g) {
+            listGhosts[ghostCount++]=g;
+        }
+        void displayGhost() const {
+            cout<<"\n---Displaying All Ghost---\n";
+            for(int i =0;i<ghostCount;i++) {
+                listGhosts[i]->display();
+                cout<<" \n ";
+            }
+            cout<<".......................\n";
+        }
+        int totalScareLevels() {
+            int sum=0;
+            for(int i=0;i<ghostCount;i++) {
+                sum += listGhosts[i]->getScarelevel();
+            }
+            return sum;
+        }
+        void visit(visitor* list,int visitorCount) {
+            int  j=0;
+            for(int i=0;i<visitorCount;i++) {
+                list[i].react(listGhosts[j%ghostCount]->getScarelevel());
+                cout<<endl;
+                j++;
+            }
+        }
+};   
+
 int main() {
-    
+    hauntedHouse hh("Bhoot Bangla", 5);
+
+    ghost* g1 = new Poltergeist("Shamveel", 5);
+    ghost* g2 = new Banshee("Moiz", 8);
+    ghost* g3 = new Shadowghost("Usman", 6);
+    ghost* g4 = new ShadowPoltergeist("Kabeer", 10);
+    ghost* g5 = new Poltergeist("Abdullah", 4);
+
+    hh.addGhost(g1);
+    hh.addGhost(g2);
+    hh.addGhost(g3);
+    hh.addGhost(g4);
+    hh.addGhost(g5);
+
+    hh.displayGhost();
+    cout << "Total Scare Level: " << hh.totalScareLevels() << endl;
+
+    visitor v1("Ali",7);
+    visitor v2("Hassan",9);
+    visitor v3("Hamza",5);
+    visitor visitors[3] = {v1,v2,v3};
+    hh.visit(visitors, 3);
+
+    delete g1;
+    delete g2;
+    delete g3;
+    delete g4;
+    delete g5;
+
+    return 0;
 }
